fb9a4a06c7ff6492d4bcfa8828de568b8260460c
Chuong Ho <chuong.git@gmail.com>
2020-04-13 21:26:14 +1000

    stop using simple ref velocity increase decrease

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,29 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
-
+#include "spline.h"
+#include "road.h"
 // for convenience
 using nlohmann::json;
 using std::string;
 using std::vector;
+
+const int EGO_KEY = -1;
+
+double max_s = 6945.554;
+
+int SPEED_LIMIT = 49;
+
+vector<int> LANE_SPEEDS = {49,49,49};
+
+double TRAFFIC_DENSITY = 0.15;
+
+int MAX_ACCEL = 10;
+
+double goal_s = max_s;
+int goal_lane = 1;
+
+Road road = Road(SPEED_LIMIT, TRAFFIC_DENSITY, LANE_SPEEDS);
 
 int main() {
   uWS::Hub h;
@@ -23,10 +41,19 @@ int main() {
   vector<double> map_waypoints_dx;
   vector<double> map_waypoints_dy;
 
+  //
+  //
+  int lane = 1;
+  int num_lanes = LANE_SPEEDS.size();
+  vector<int> ego_config = {SPEED_LIMIT, num_lanes, (int) goal_s, goal_lane, MAX_ACCEL};
+  //
+  //
+  road.add_ego(lane, 0, ego_config);
+
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
   // The max s value before wrapping around the track back to 0
-  double max_s = 6945.554;
+
 
   std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
 
@@ -50,8 +77,12 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  h.onMessage([&map_waypoints_x,
+                &map_waypoints_y,
+                &map_waypoints_s,
+               &map_waypoints_dx,
+               &map_waypoints_dy,
+               &lane]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -86,19 +117,184 @@ int main() {
 
           // Sensor Fusion Data, a list of all other cars on the same side 
           //   of the road.
-          auto sensor_fusion = j[1]["sensor_fusion"];
+          vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
 
           json msgJson;
 
+          vector<double> ptsx;
+          vector<double> ptsy;
+          double ref_vel = car_speed;
+
+          int prev_size = previous_path_x.size();
+          
+          if (prev_size > 0)
+          {
+            car_s = end_path_s;
+          }
+
+          Vehicle ego = road.get_ego();
+          ego.update(car_x, car_y, car_s, car_d, car_speed, car_yaw);
+          
+          vector<Vehicle> other_cars;
+          vector<int> other_ids;
+
+          for (int i = 0; i < sensor_fusion.size(); ++i)
+          {
+            other_ids.push_back(sensor_fusion[i][0]);
+            double x = sensor_fusion[i][1];
+            double y = sensor_fusion[i][2];
+            double vx = sensor_fusion[i][3];
+            double vy = sensor_fusion[i][4];
+            double s = sensor_fusion[i][5];
+            double d =sensor_fusion[i][6];
+            double v = sqrt(vx*vx+vy*vy);
+            Vehicle new_car;
+            new_car.update(x, y, s, d, v, atan2(vy, vx));
+          }
+
+          bool too_close = false;
+
+          // for (int i = 0; i < sensor_fusion.size(); ++i)
+          // {
+          //   float d = sensor_fusion[i][0];
+          //   if ((d < 2 + 4*lane + 4 + 2) && (d > 4*lane + 4) )
+          //   {
+          //     double vx = sensor_fusion[i][3];
+          //     double vy = sensor_fusion[i][4];
+
+          //     double check_speed = sqrt(vx*vx + vy*vy);
+          //     double check_car_s = sensor_fusion[i][5];
+
+          //     check_car_s += ((double) prev_size * .02 * check_speed);
+
+          //     if ((check_car_s > car_s) && (check_car_s - car_s < 30))
+          //     {
+          //       //ref_vel = 29.5;
+          //       too_close = true;
+          //       //std::
+          //     }
+          //   }
+          // // }
+
+          // if (too_close)
+          // {
+          //   ref_vel -= .224;
+          // } else if (ref_vel < 49.5)
+          // {
+          //   ref_vel += .224;
+          // }
+
+          int next_wp = -1;
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+          
+
+          if (prev_size <= 1)
+          {
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);          
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }
+          else {
+            // create a tangent line
+            ref_x = previous_path_x[prev_size - 1];
+            ref_y = previous_path_y[prev_size - 1];
+
+            double ref_x_prev = previous_path_x[prev_size - 2];
+            double ref_y_prev = previous_path_y[prev_size - 2];
+
+            ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
           vector<double> next_x_vals;
           vector<double> next_y_vals;
+          
+          vector<double> next_wp0 = getXY(car_s + 30, 2.0 +4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s + 60, 2.0 +4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s + 90, 2.0 +4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
 
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          for (int i = 0; i < ptsx.size(); ++i)
+          {
+            //shift car reference angle to 0 degrees
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+
+            ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y*sin(0-ref_yaw));
+            ptsy[i] = (shift_x * sin(0-ref_yaw) + shift_y*cos(0-ref_yaw));
+          }
           /**
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
 
+          tk::spline s;
+          s.set_points(ptsx, ptsy);
 
+          // vector<double> next_x_vals;
+          // vector<double> next_y_vals;
+
+          for (int i = 0; i < previous_path_x.size(); ++i)
+          {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }          
+
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = pow(pow(target_x, 2.0) + pow(target_y, 2.0), 0.5);
+
+          double x_add_on = 0.0;
+          
+          
+          for (int i = 1; i <= 50 - previous_path_x.size(); ++i)
+          {
+            double N = target_dist / (0.02 * ref_vel / 2.24);
+            double x_point = x_add_on + target_x / N;
+            double y_point = s(x_point);
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            // transforming back
+            x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+            y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
+          
+          // vector<double> next_x_vals, next_y_vals;
+          // double dist_inc = 0.3;
+          // for (int i = 0; i < 50; ++i) {
+          //   double next_s = car_s + (i+1)*dist_inc;
+          //   double next_d = 6;
+            
+          //   vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          //   next_x_vals.push_back(xy[0]);
+          //   next_y_vals.push_back(xy[1]);
+          // }
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
